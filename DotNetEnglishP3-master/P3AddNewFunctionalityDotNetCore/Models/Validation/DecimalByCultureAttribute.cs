03c0b91c9b6a065fc398839cf62239f8604d24c5
domblocs-dev <domblocsms@outlook.fr>
2026-03-27 07:36:09 +0100

    Le prix doit être un nombre décimal dans la culture

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Validation/DecimalByCultureAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace P3AddNewFunctionalityDotNetCore.Models.Validation
+{
+    public class DecimalByCultureAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Decimal.TryParse(value.ToString(), NumberStyles.Number, CultureInfo.CurrentCulture, out decimal decimalValue);
+            if (decimalValue.ToString() == value.ToString())
+                return ValidationResult.Success;
+            else
+                return new ValidationResult(ErrorMessageString);
+
+        }
+    }
+}
